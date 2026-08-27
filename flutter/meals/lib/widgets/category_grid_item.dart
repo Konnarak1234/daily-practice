c6bab4b84b410355f694dd90ed62bef68e2e505b
Konnarak Yahath <134821125+Konnarak1234@users.noreply.github.com>
2026-08-27 11:29:46 +0700

    make meal-category tapable, for navigation purpose

--- flutter/meals/lib/widgets/category_grid_item.dart
+++ flutter/meals/lib/widgets/category_grid_item.dart
@@ -8,22 +8,26 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withAlpha(125),
-            category.color.withAlpha(220),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).colorScheme.primary,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withAlpha(125),
+              category.color.withAlpha(220),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
